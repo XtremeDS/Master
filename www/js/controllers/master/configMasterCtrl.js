e9297beb4bae7823619ce1a4ab163ffabfa9d375
Emanuel Coelho <emanuel@wayacross.pt>
2015-06-10 00:02:42 +0100

    Master Config User config views

--- www/js/controllers/master/configMasterCtrl.js
+++ www/js/controllers/master/configMasterCtrl.js
@@ -1,4 +1,6 @@
-app.controller('ConfigMasterCtrl', function ($scope, AppService, CommonStubService) {
+app.controller('ConfigMasterCtrl', function ($scope, AppService, CommonStubService, MasterConfig) {
+	
+	$scope.masterInfo = MasterConfig;
 
 	$scope.registerInformation = {
 		password: false,
@@ -9,28 +11,7 @@ app.controller('ConfigMasterCtrl', function ($scope, AppService, CommonStubServi
 	// Set userLogged - 0:Not logged 1:Logged
     $scope.isLogged = AppService.getIsLogged();
 
-	$scope.masterInfo = {
-		accessData:{
-			email:false,
-			repeatEmail:false,
-			password:false,
-			repeatPassword:false
-		},
-		personalData:{
-			name:false,
-			phone:false,
-			zipCode:false,
-			country:false
-		},
-		association:{
-			name:false,
-			videoLink:false,
-			pageLink:false,
-			description:false
-		}
-	};	
-
 	$scope.doMasterLogout = function() {
-		console.log('Logou pressed');
+		console.log('Logout pressed');
 	};
 });
--- www/js/controllers/master/masterUserConfigCtrl.js
+++ www/js/controllers/master/masterUserConfigCtrl.js
@@ -1,25 +1,28 @@
-app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService, CommonStubService) {
+app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService, CommonStubService, MasterConfig) {
+	
+	$scope.masterInfo = MasterConfig;
+	$scope.isFormValid = false;
 	
 	$scope.registerMasterInfo = function(sbmitedMasterInfo, isLogged){
 		//In case is logged is a change data
 	}
 	
 	/* Choose Country Modal */
-    $ionicModal.fromTemplateUrl('templates/modals/choose-country.html', function(modal) {
-      $scope.countryModal = modal;
-    }, {
-      scope: $scope,
-      animation: 'slide-in-up'
-    });
+	$ionicModal.fromTemplateUrl('templates/modals/choose-country.html', function(modal) {
+		$scope.countryModal = modal;
+	}, {
+		scope: $scope,
+		animation: 'slide-in-up'
+	});
 	
-  	// Open choose country modal
-    $scope.newTask = function() {
-      $scope.countryModal.show();
-    };
+	// Open choose country modal
+	$scope.newTask = function() {
+		$scope.countryModal.show();
+	};
 
-    // Close country modal
-    $scope.closeNewTask = function() {
-      $scope.countryModal.hide();
-    };
+	// Close country modal
+	$scope.closeNewTask = function() {
+		$scope.countryModal.hide();
+	};
 	
 });
