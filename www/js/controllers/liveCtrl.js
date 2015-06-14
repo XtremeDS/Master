1a9920dfed48b38f434427a1f853f8acf4ba5e89
JoaoLeiria <godzilaa14eu@gmail.com>
2015-06-14 18:29:12 +0100

    Play

--- www/js/controllers/liveCtrl.js
+++ www/js/controllers/liveCtrl.js
@@ -34,11 +34,13 @@ app.controller('LiveCtrl', function ($scope, $ionicLoading, $timeout, $ionicPopu
         $scope.modalSeeActivePerks.show();
     };
 
+    window.block = function (container, stop) {
+        $scope.blockUI(container, stop);
+    };
     $scope.blockUI = function (container, stop) {
         if ($($(container)).find('.blockUI').length > 0) $(container).find('.blockUI').remove();
-
         if (!stop) {
-            $(container).append('<div class="blockUI"><div class="play-button"><a class="button button-icon icon ion-play custom-icon"></a></div></div>');
+            $(container).append('<div class="blockUI"><div class="play-button"><a class="button button-icon icon ion-play custom-icon" onclick="window.block(\'#view-live\',true)"></a></div></div>');
         }
     };
 
