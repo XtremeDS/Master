bc255cfe91c314f9384905e6c6d672f641e8bbc0
Josip Bagarić <baga@Macsus.lan>
2015-06-14 12:29:43 +0100

    Implementing the events and faction management

--- www/js/controllers/eventsCtrl.js
+++ www/js/controllers/eventsCtrl.js
@@ -1,7 +1,7 @@
 /**
  * Created by joaosilva on 25/05/15.
  */
-app.controller('EventsCtrl', function ($scope, AppService, CommonStubService) {
+app.controller('EventsCtrl', function ($scope, AppService, MasterStubService) {
     $scope.data = {
         showDelete: false,
         showReorder: false,
@@ -72,14 +72,14 @@ app.controller('EventsCtrl', function ($scope, AppService, CommonStubService) {
     };
 
     $scope.getAllEvents = function(){
-        console.log("Entras!");
-        CommonStubService.getAllEvents()
+        console.log("Searching for events!");
+        MasterStubService.getAllMasterEvents()
             .success(function (data) {
-                $scope.getAllEventsResult = data.response;
+                $scope.items = data.response;
                 console.log($scope.getAllEventsResult);
             })
             .error(function (error) {
-                $scope.getAllEventsResult = 'Unable to load data: ' + error;
+                $scope.items = 'Unable to load data: ' + error;
                 console.log($scope.getAllEventsResult);
             });
         };
@@ -100,12 +100,12 @@ app.controller('EventsCtrl', function ($scope, AppService, CommonStubService) {
     };
 
     //TODO : Read data from back-end instead of dummy data
-    $scope.items = [{id: 1, name: "Event 1", location: "Leiria"},
+    /*$scope.items = [{id: 1, name: "Event 1", location: "Leiria"},
         {id: 2, name: "Event 2", location: "Lisboa"},
         {id: 3, name: "Event 3", location: "Porto"},
         {id: 4, name: "Event 4", location: "Aveiro"},
         {id: 5, name: "Event 5", location: "Coimbra"},
         {id: 6, name: "Event 6", location: "Braga"},
         {id: 7, name: "Event 7", location: "Santarém"},
-        {id: 8, name: "Event 8", location: "Viana do Castelo"}];
+        {id: 8, name: "Event 8", location: "Viana do Castelo"}];*/
 });
