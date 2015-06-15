abde5fc5c927820355a6f8d07f269002651cb9a5
KlaudioS <klaudio.ads@gmail.com>
2015-06-15 18:09:35 +0100

    Create and update master with validation

--- www/js/controllers/master/masterUserConfigCtrl.js
+++ www/js/controllers/master/masterUserConfigCtrl.js
@@ -6,11 +6,55 @@ app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService
 	
 	$scope.registerMasterInfo = function(submitedForm){
 		//In case is logged is a change data
-		console.log(submitedForm.accessData.email);
-		console.log();
-		console.log(MasterStubService.createMaster(submitedForm.accessData.email, submitedForm.accessData.password,  submitedForm.personalData.name, null,
-				submitedForm.personalData.phone, null, submitedForm.personalData.zipcode, submitedForm.personalData.country.countryName, submitedForm.association.description, submitedForm.association.pageLink, submitedForm.association.videoLink ));
-		
+	
+	
+	var result;
+	var login;
+
+			MasterStubService.loginCheckMaster()
+			.success(function (data) {
+				login = data.response;
+			})
+			.error(function (error) {
+				$scope.loginCheckResult = 'Unable to load data: ' + error;
+			});
+
+			if(login == 0){
+				MasterStubService.createMaster(submitedForm.accessData.email, submitedForm.accessData.password,"teste",null,
+				submitedForm.personalData.phone, "Rua da estrada", submitedForm.personalData.zipCode, submitedForm.personalData.country.countryName, submitedForm.association.description, 
+				submitedForm.association.pageLink, submitedForm.association.videoLink)
+					.success(function (data) {
+						if(data.response == 0){
+								for(var key in data.errors) {
+								var value = data.errors[key];
+								console.log(value);	
+							}
+						}
+				})
+					.error(function (error) {
+					$scope.createMasterResult = 'Unable to load data: ' + error;
+				});
+			}else{
+				MasterStubService.updateMasterPersonalConfig(0, 2, "teste3", null,
+				submitedForm.personalData.phone, "Rua Alterada", submitedForm.personalData.zipCode, submitedForm.personalData.country,submitedForm.association.description, 
+				submitedForm.association.pageLink, submitedForm.association.videoLink)
+				.success(function (data){
+					console.log(data);
+					$scope.updateMasterConfigResult = data.response;
+					if(data == 0){
+						Console.log("Error updating");
+					}
+				})
+				.error(function (error) {
+					$scope.updateMasterConfigResult = 'Unable to load data: ' + error;
+				});
+			}
+
+
+/*		------TODO 
+		IF DUPLICATE 
+		IF LOGED IN UPDATE
+*/
 	}
 	
 	/* Choose Country Modal */
