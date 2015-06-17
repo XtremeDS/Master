a1ae3d990cbf2eef9824c871d4bd67f7d2d2cf1b
Emanuel Coelho <emanuel@wayacross.pt>
2015-06-17 23:13:00 +0100

    Added Address field (Model)

--- www/js/factories/masterConfigFactory.js
+++ www/js/factories/masterConfigFactory.js
@@ -11,7 +11,8 @@ app.factory('MasterConfig', function(){
 			country:{
 				country:'pt',
 				countryName:'Portugal'
-			}
+			},
+			address:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
 		},
 		association:{
 			name:"Airsot",
