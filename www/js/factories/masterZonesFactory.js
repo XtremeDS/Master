956fa95919abb7aef3bba937c564769e57986242
Emanuel Coelho <emanuel@wayacross.pt>
2015-06-22 00:56:45 +0100

    Added MasterZoneFactories

---
+++ www/js/factories/masterZonesFactory.js
@@ -0,0 +1,25 @@
+app.factory('MasterZones', function(){
+	/* DUMMY DATA SO FAR */
+	return [
+		{
+			name: "Map 1",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+		},
+		{
+			name: "Map 2",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+		},
+		{
+			name: "Map 3",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+		},
+		{
+			name: "Map 4",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+		},
+		{
+			name: "Map 5",
+			description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+		}
+	];
+});
