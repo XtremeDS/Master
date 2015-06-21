b04057a9da35dc4ef7e4e102ac8549395de08bc2
Emanuel Coelho <emanuel@wayacross.pt>
2015-06-22 00:29:27 +0100

    Added Inforation to prevent actions without loading the data. The steps are commented because I (Emanuel) don't have access to School's VPN.

--- www/js/controllers/master/configMasterCtrl.js
+++ www/js/controllers/master/configMasterCtrl.js
@@ -1,17 +1,43 @@
-app.controller('ConfigMasterCtrl', function ($scope, AppService, CommonStubService, MasterConfig) {
+app.controller('ConfigMasterCtrl', function ($scope, AppService, CommonStubService, MasterConfig, $ionicLoading, MasterStubService) {
 	
-	$scope.masterInfo = MasterConfig;
-
-	$scope.registerInformation = {
-		password: false,
-		repeatPassword: false,
-		repeatEmail: false
-	}
+	/*$scope.showLoadding = $ionicLoading.show({
+		template: '<ion-spinner icon="android"></ion-spinner>'
+		});
 	
-	// Set userLogged - 0:Not logged 1:Logged
-    $scope.isLogged = 1;
+		MasterStubService.loginCheckMaster().success(function (data) {
+		// Set userLogged - 0:Not logged 1:Logged
+		$scope.isLogged = 1;
+		$ionicLoading.hide();
+		}).error(function (error) {
+		// Set userLogged - 0:Not logged 1:Logged
+		$scope.isLogged = 0;
+		$ionicLoading.hide();
+		$scope.showFailLoginAlert();
+		});*/
+		
+		/* RIGHT NOW I HAVE TO FORCE IT TO BE LOGGED IN */
+		$scope.isLogged = 1;
 
-	$scope.doMasterLogout = function() {
-		console.log('Logout pressed');
-	};
-});
+		$scope.registerInformation = {
+			password: false,
+			repeatPassword: false,
+			repeatEmail: false
+		}
+
+		$scope.doMasterLogout = function() {
+			console.log('Logout pressed');
+		};
+	
+		/* Fail to get MasterInfData */
+		// An alert dialog
+		$scope.showFailLoginAlert = function() {
+			var alertPopup = $ionicPopup.alert({
+				cssClass: "alert-danger",
+				title: 'Something is not right',
+				template: 'Please try again'
+			});
+			alertPopup.then(function(res) {
+		  
+			});
+		};
+	});
--- www/js/controllers/master/masterUserConfigCtrl.js
+++ www/js/controllers/master/masterUserConfigCtrl.js
@@ -1,8 +1,47 @@
-app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService, MasterStubService, MasterConfig, Countries) {
+app.controller('MasterUserConfigCtrl', function ($scope, $state, $ionicHistory, $ionicPopup, $ionicLoading, $ionicModal, AppService, MasterStubService, MasterConfig, Countries) {	
 	
 	$scope.masterInfo = MasterConfig;
 	$scope.isFormValid = false;
 	$scope.countries = Countries;
+	
+	/* FOR NOW I HAVE TO USE DUMMY DATA */
+	
+    /*$scope.showLoadding = $ionicLoading.show({
+         template: '<ion-spinner icon="android"></ion-spinner>'
+    });
+	
+	MasterStubService.loginCheckMaster().success(function (data) {
+		console.log(data);
+		login = data.response;
+		
+		/* Assign Recieved Data To MasterFactory */
+		/* TODO --- CALL FUNCTION HERE TO ASSIGN *
+		
+		$ionicLoading.hide();
+	}).error(function (error) {
+		$scope.loginCheckResult = 'Unable to load data: ' + error;
+		console.log(error);
+		$ionicLoading.hide();
+		$scope.showFailLoginAlert();
+	});*/
+	
+	
+	/* Fail to get MasterInfData */
+    // An alert dialog
+    $scope.showFailLoginAlert = function() {
+      var alertPopup = $ionicPopup.alert({
+		  cssClass: "alert-danger",
+        title: 'Something is not right',
+        template: 'Something went wrong while getting your data.\n Please go back and try again'
+      });
+      alertPopup.then(function(res) {
+        console.log('Thank you for not eating my delicious ice cream cone');
+		$ionicHistory.nextViewOptions({
+		    disableBack: true
+		 });
+		$state.go('app.masterconfig');
+      });
+    };
 	
 	$scope.isMFormInvalid = function (submitedForm){
 		return submitedForm.$invalid;
@@ -19,14 +58,6 @@ app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService
 		if(submitedForm.$valid){
 			
 		}
-
-		MasterStubService.loginCheckMaster()
-		.success(function (data) {
-			login = data.response;
-		})
-		.error(function (error) {
-			$scope.loginCheckResult = 'Unable to load data: ' + error;
-		});
 
 		if(login == 0){
 			MasterStubService.createMaster(submitedForm.accessData.email, submitedForm.accessData.password,"teste",null,
@@ -58,12 +89,6 @@ app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService
 				$scope.updateMasterConfigResult = 'Unable to load data: ' + error;
 			});
 		}
-
-
-		/*		------TODO 
-		IF DUPLICATE 
-		IF LOGED IN UPDATE
-		*/
 	}
 	
 	/* Choose Country Modal */
@@ -100,5 +125,4 @@ app.controller('MasterUserConfigCtrl', function ($scope, $ionicModal, AppService
 		
 		$scope.countryModal.hide();
 	}
-	
 });
